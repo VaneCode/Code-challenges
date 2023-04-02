0b20b35cb7a6a1bc42ee13874df1ab06c4533f2d
Vanessa Bonito <vanebonito@hotmail.com>
2023-04-01 19:09:31 -0500

    Add slution to binarySarch.rb

--- binarySearch.rb
+++ binarySearch.rb
@@ -1,1 +1,14 @@
-
+def search(nums, target, min , max)
+  if(min > max)
+    return -1
+  else 
+    @mid = (min + max) / 2
+    if(nums[@mid] == target)
+      return @mid
+    elsif (nums[@mid]>target)
+      return search(nums, target, @mid+1, max)
+    elsif (nums[@mid]<target)
+      return search(nums, target, min, @mid-1)
+    end
+  end
+end
